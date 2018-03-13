7c0566cf1ab31832dd99da5c994f6c27387c1717
Marcin Konieczny <marcin_k@coni.pl>
2018-03-14 00:18:51 +0100

    Poprawki wizualne

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,22 @@
-INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica)
-VALUES
-  ('Marcin', 'Marcinowy', 'Poznań', 'marcin@co.pl', '54', 'Marcinex', '8888888', '666666666', '9', 'os. B.
-  Chrobrego');
-INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica)
-VALUES ('Tomasz', 'Tomkowski', 'Gdańsk', 'tomasz@tomex.pl', '', 'Tomanex', '99999', '777777777', '10', 'Szatana');
-INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica)
-VALUES ('Kasia', 'Kasiunia', 'Piła', 'kasiaaaa@buziaczek.pl', '10', 'Kasiex', '1111111', '444444444', '8', 'Kasiowska');
-INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica)
-VALUES
-  ('Krasula', 'Krasulska', 'Kołobrzeg', 'krowa@muu.pl', '1', 'Krowex', '333333333', '111111111', '2', 'Szarlatana');
-INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica)
-VALUES
-  ('Zygmunt', 'Zygowski', 'Zakopane', 'zyga@zygeo.pl', '1', 'Zygeo', '7819870987', '+489998765', '290', 'Długa');
+INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica,
+                             kod_pocztowy)
+VALUES ('Marcin', 'Marcinowy', 'Poznań', 'marcin@co.pl', '54', 'Marcinex', '8888888', '666666666', '9', 'os. B.
+  Chrobrego', '60-897');
+INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica,
+                             kod_pocztowy)
+VALUES ('Tomasz', 'Tomkowski', 'Gdańsk', 'tomasz@tomex.pl', '', 'Tomanex', '99999', '777777777', '10', 'Szatana', '67-987');
+INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica,
+                             kod_pocztowy)
+VALUES ('Kasia', 'Kasiunia', 'Piła', 'kasiaaaa@buziaczek.pl', '10', 'Kasiex', '1111111', '444444444', '8',
+                 'Kasiowska', '67-987');
+INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica,
+                             kod_pocztowy)
+VALUES ('Krasula', 'Krasulska', 'Kołobrzeg', 'krowa@muu.pl', '1', 'Krowex', '333333333', '111111111', '2', 'Szarlatana',
+                   '67-987');
+INSERT INTO offers.inwestor (imie, nazwisko, miasto, email, nr_lokalu, nazwa, nip, numertelefonu, nr_posesji, ulica,
+                             kod_pocztowy)
+VALUES ('Zygmunt', 'Zygowski', 'Zakopane', 'zyga@zygeo.pl', '1', 'Zygeo', '7819870987', '+489998765', '290', 'Długa',
+                   '67-987');
 INSERT INTO offers.skladnik_oferty (opis, nazwa_uslugi, jednostka, kategoria_skladnika)
 VALUES ('Badania gruntu', 'Badanie gruntu', 'szt.', 'FIELD_TESTS');
 INSERT INTO offers.skladnik_oferty (opis, nazwa_uslugi, jednostka, kategoria_skladnika)
